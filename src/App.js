fbb7d1ddee868a9337f7415ec7ff4d4662ac64c3
畔上　高典 <takanori.azegami.ta@ultmarc.jp>
2023-04-08 10:35:07 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getallPokemon } from './utils/pokemon.js';
 
 function App() {
+	const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchPokemonData = async() => {
+			//全てのポケモンデータを取得
+			let res = await getallPokemon(initialURL);
+			console.log(res);
+			setLoading(false);
+		};
+		fetchPokemonData();
+	}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			{ loading ? (
+				<h1>ロード中・・・</h1>
+			) : (
+				<>
+					<h1>ポケモンデータを取得しました</h1>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
