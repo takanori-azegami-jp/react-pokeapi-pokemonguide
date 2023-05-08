da0c18248ff821d2a3a0d330194e9b81adae58b5
畔上高典 <t.azegami213@gmail.com>
2023-05-09 01:02:54 +0900

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,17 +5,19 @@ const Navbar = () => {
   return (
     <div>
       <nav>
-        ポケモンずかん{'　'}
-        <small>
-          Powered by{' '}
-          <a
-            href="https://pokeapi.co/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            pokeAPI
-          </a>
-        </small>
+        <div>ポケモンずかん</div>
+        <div>
+          <small>
+            Powered by{' '}
+            <a
+              href="https://pokeapi.co/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              pokeAPI
+            </a>
+          </small>
+        </div>
       </nav>
     </div>
   );
